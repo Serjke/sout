d25b3f5a0f68d8d7dea47a38e2906062b1820617
Serjos <sk-2006@mail.ru>
2015-03-23 22:59:28 +0300

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,13 @@
+/**
+ * Created by Serjos on 23.03.15.
+ */
+$(document).ready(function () {
+    $('.slider3').bxSlider({
+        adaptiveHeight: true,
+        touchEnabled: true,
+        pager: true,
+        controls: true,
+        auto: false,
+        slideMargin: 1
+    });
+});
