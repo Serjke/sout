2084002e4147436c3eba4769b5e993d8961d765d
Sergey <komarov.s@drofa.ru>
2015-03-24 11:20:17 +0300

    css + html fix

--- js/main.js
+++ js/main.js
@@ -2,6 +2,12 @@
  * Created by Serjos on 23.03.15.
  */
 $(document).ready(function () {
+	$('.show').click(function(e) {
+		e.preventDefault();
+		var $this = $(this);
+		$this.next('div.hidden').slideToggle();
+	});
+	if($.isFunction($.fn.bxSlider)){
     $('.slider3').bxSlider({
         adaptiveHeight: true,
         touchEnabled: true,
@@ -10,4 +16,5 @@ $(document).ready(function () {
         auto: false,
         slideMargin: 1
     });
+    }
 });
