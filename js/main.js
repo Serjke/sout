7b8a9ceb2f976e96682d07e822aa690b289c8ae0
Sergey <komarov.s@drofa.ru>
2015-03-27 16:13:28 +0300

    add show/hide list

--- js/main.js
+++ js/main.js
@@ -2,10 +2,18 @@
  * Created by Serjos on 23.03.15.
  */
 $(document).ready(function () {
-	$('.show').click(function(e) {
+    $('.show').click(function(e) {
+        e.preventDefault();
+        var $this = $(this);
+        $this.next('div.hidden').slideToggle();
+    });
+   
+    $('.show-list').click(function(e) {
 		e.preventDefault();
 		var $this = $(this);
-		$this.next('div.hidden').slideToggle();
+        var hBlock = $(this).prev('ul.hidden');
+         $(this).text(hBlock.is(':visible') ? 'Показать больше' : 'Скрыть');
+		hBlock.slideToggle();
 	});
 	if($.isFunction($.fn.bxSlider)){
     $('.slider3').bxSlider({
